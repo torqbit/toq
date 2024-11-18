263a89e9ef7d03ab35642d1ed084fda2ca93f62a
mehrab-alam <mehrab2000alam@gmail.com>
2024-11-18 12:27:05 +0530

    updated the nav bar component

--- src/components/Marketing/LandingPage/NavBar/NavBar.tsx
+++ src/components/Marketing/LandingPage/NavBar/NavBar.tsx
@@ -10,46 +10,7 @@ import MobileNav from "./SideNavBar";
 const NavBar: FC<INavBarProps> = ({ user, items, brand, showThemeSwitch, activeTheme, isMobile }): ReactElement => {
   return (
     <>
-      <div className={styles.navBarContainer}>
-        <nav>
-          <Link href={"/"} aria-label="Go back to landing page">
-            <Flex align="center" gap={5}>
-              {typeof brand.logo === "string" ? (
-                <Image src={brand.logo} height={40} width={40} alt={"logo"} />
-              ) : (
-                brand.logo
-              )}
-              <h1 className="font-brand">{brand.name}</h1>
-            </Flex>
-          </Link>
-          <div className={styles.link_wrapper}>
-            {items.length === 0 ? (
-              <div></div>
-            ) : (
-              <ul>
-                {items.map((navigation, i) => {
-                  return (
-                    <li key={i}>
-                      <Link href={navigation.link} aria-label={`link to ${navigation.title} page`}>
-                        {navigation.title}
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul>
-            )}
-            <Flex align="center" gap={20}>
-              {showThemeSwitch && <ThemeSwitch activeTheme={activeTheme} />}
-
-              <Link href={user ? `/dashboard` : `/login`} aria-label="Get started">
-                <Button type="primary">{user ? "Go to Dashboard" : "Get Started"}</Button>
-              </Link>
-            </Flex>
-          </div>
-        </nav>
-      </div>
-
-      <>
+      {isMobile ? (
         <MobileNav
           items={items}
           showThemeSwitch={showThemeSwitch}
@@ -57,7 +18,46 @@ const NavBar: FC<INavBarProps> = ({ user, items, brand, showThemeSwitch, activeT
           brand={brand}
           isMobile={isMobile}
         />
-      </>
+      ) : (
+        <div className={styles.navBarContainer}>
+          <nav>
+            <Link href={"/"} aria-label="Go back to landing page">
+              <Flex align="center" gap={5}>
+                {typeof brand.logo === "string" ? (
+                  <Image src={brand.logo} height={40} width={40} alt={"logo"} />
+                ) : (
+                  brand.logo
+                )}
+                <h1 className="font-brand">{brand.name}</h1>
+              </Flex>
+            </Link>
+            <div className={styles.link_wrapper}>
+              {items.length === 0 ? (
+                <div></div>
+              ) : (
+                <ul>
+                  {items.map((navigation, i) => {
+                    return (
+                      <li key={i}>
+                        <Link href={navigation.link} aria-label={`link to ${navigation.title} page`}>
+                          {navigation.title}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+              <Flex align="center" gap={20}>
+                {showThemeSwitch && <ThemeSwitch activeTheme={activeTheme} />}
+
+                <Link href={user ? `/dashboard` : `/login`} aria-label="Get started">
+                  <Button type="primary">{user ? "Go to Dashboard" : "Get Started"}</Button>
+                </Link>
+              </Flex>
+            </div>
+          </nav>
+        </div>
+      )}
     </>
   );
 };
--- src/components/Marketing/LandingPage/NavBar/SideNavBar.tsx
+++ src/components/Marketing/LandingPage/NavBar/SideNavBar.tsx
@@ -7,7 +7,7 @@ import { INavBarProps } from "@/types/landing/navbar";
 import ThemeSwitch from "@/components/ThemeSwitch/ThemeSwitch";
 import Hamburger from "hamburger-react";
 
-const MobileNav: FC<INavBarProps> = ({ items, showThemeSwitch, activeTheme, brand, isMobile }) => {
+const MobileNav: FC<INavBarProps> = ({ items, showThemeSwitch, activeTheme, brand }) => {
   const [showSideNav, setSideNav] = useState(false);
 
   const onAnchorClick = () => {
@@ -15,70 +15,66 @@ const MobileNav: FC<INavBarProps> = ({ items, showThemeSwitch, activeTheme, bran
   };
   return (
     <>
-      {isMobile && (
-        <>
-          <section className={styles.sideNaveContainer}>
-            <Drawer
-              classNames={{ header: styles.drawerHeader }}
-              title={
-                <div className={styles.drawerTitle}>
-                  <Link href={"/"} aria-label="Go back to landing page">
-                    <Flex align="center" gap={5}>
-                      <Image src={`${brand.logo}`} height={40} width={40} alt={"logo"} loading="lazy" />
-                      <h1 className="font-brand">{brand.name}</h1>
-                    </Flex>
-                  </Link>
-                  {showSideNav && showThemeSwitch && <ThemeSwitch activeTheme={activeTheme} />}
-                </div>
-              }
-              placement="left"
-              width={300}
-              closable={false}
-              onClose={onAnchorClick}
-              open={showSideNav}
-            >
-              <div className={styles.menuDrawer}>
-                {items &&
-                  items.map((item, i) => {
-                    return (
-                      <div
-                        key={i}
-                        className={styles.drawerMenuItems}
-                        onClick={() => item.title === "Courses" && onAnchorClick()}
-                      >
-                        {item.title === "Courses" ? (
-                          <a href={item.link} className={styles.menuTitle} aria-label={`link to ${item.title}`}>
-                            <div>{item.title}</div>
-                          </a>
-                        ) : (
-                          <Link key={i} href={item.link} aria-label={`link to ${item.title}`}>
-                            {item.title}
-                          </Link>
-                        )}
-                      </div>
-                    );
-                  })}
-              </div>
-            </Drawer>
-          </section>
-          <Link href={"/"} className={styles.platformNameLogo}>
-            <Flex align="center" gap={5}>
-              <Image src={`${brand.logo}`} height={40} width={40} alt={"logo"} loading="lazy" />
-              <h4 className="font-brand">{brand.name}</h4>
-            </Flex>
-          </Link>
-          <div role="button" className={styles.hamburger} aria-label="Toggle menu">
-            <Hamburger
-              rounded
-              direction="left"
-              toggled={showSideNav}
-              onToggle={(toggle: boolean | ((prevState: boolean) => boolean)) => {
-                setSideNav(toggle);
-              }}
-            />
+      <section className={styles.sideNaveContainer}>
+        <Drawer
+          classNames={{ header: styles.drawerHeader }}
+          title={
+            <div className={styles.drawerTitle}>
+              <Link href={"/"} aria-label="Go back to landing page">
+                <Flex align="center" gap={5}>
+                  <Image src={`${brand.logo}`} height={40} width={40} alt={"logo"} loading="lazy" />
+                  <h1 className="font-brand">{brand.name}</h1>
+                </Flex>
+              </Link>
+              {showSideNav && showThemeSwitch && <ThemeSwitch activeTheme={activeTheme} />}
+            </div>
+          }
+          placement="left"
+          width={300}
+          closable={false}
+          onClose={onAnchorClick}
+          open={showSideNav}
+        >
+          <div className={styles.menuDrawer}>
+            {items &&
+              items.map((item, i) => {
+                return (
+                  <div
+                    key={i}
+                    className={styles.drawerMenuItems}
+                    onClick={() => item.title === "Courses" && onAnchorClick()}
+                  >
+                    {item.title === "Courses" ? (
+                      <a href={item.link} className={styles.menuTitle} aria-label={`link to ${item.title}`}>
+                        <div>{item.title}</div>
+                      </a>
+                    ) : (
+                      <Link key={i} href={item.link} aria-label={`link to ${item.title}`}>
+                        {item.title}
+                      </Link>
+                    )}
+                  </div>
+                );
+              })}
           </div>
-        </>
-      )}
+        </Drawer>
+      </section>
+      <Link href={"/"} className={styles.platformNameLogo}>
+        <Flex align="center" gap={5}>
+          <Image src={`${brand.logo}`} height={40} width={40} alt={"logo"} loading="lazy" />
+          <h4 className="font-brand">{brand.name}</h4>
+        </Flex>
+      </Link>
+      <div role="button" className={styles.hamburger} aria-label="Toggle menu">
+        <Hamburger
+          rounded
+          direction="left"
+          toggled={showSideNav}
+          onToggle={(toggle: boolean | ((prevState: boolean) => boolean)) => {
+            setSideNav(toggle);
+          }}
+        />
+      </div>
     </>
   );
 };
