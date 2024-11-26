732058789c69aa184e8f87e84345feceec741c62
Shad <shad.amezng@gmail.com>
2024-11-26 12:01:56 +0530

    created types for the bunny CMS

--- src/pages/api/v1/admin/config/cms/regions.ts
+++ src/pages/api/v1/admin/config/cms/regions.ts
@@ -4,32 +4,32 @@ import { errorHandler } from "@/lib/api-middlewares/errorHandler";
 import { z } from "zod";
 import { APIServerError } from "@/types/cms/apis";
 import { ContentManagementService } from "@/services/cms/ContentManagementService";
+import { withUserAuthorized } from "@/lib/api-middlewares/with-authorized";
 
 const cmsAccess = z.object({
-    accessKey: z.string().min(2, "Access key is required"),
-    provider: z.string().min(2, "Provider is required"),
+  accessKey: z.string().min(2, "Access key is required"),
+  provider: z.string().min(2, "Provider is required"),
 });
 
 type CMSConfig = z.infer<typeof cmsAccess>;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-        const body = await req.body;
-        const accessConfig: CMSConfig = cmsAccess.parse(body);
-        const cms = new ContentManagementService().getCMS(accessConfig.provider);
-        const result = await cms.listReplicationRegions();
+  try {
+    const body = await req.body;
+    const accessConfig: CMSConfig = cmsAccess.parse(body);
+    const cms = new ContentManagementService().getCMS(accessConfig.provider);
+    const result = await cms.listReplicationRegions();
 
-        console.log(result)
+    console.log(result);
 
-        return res.status(200).json({
-            success: true,
-            messsage: "Fetched the replicated regions successfully",
-            regions: result
-        });
-
-    } catch (error) {
-        return errorHandler(error, res);
-    }
+    return res.status(200).json({
+      success: true,
+      messsage: "Fetched the replicated regions successfully",
+      regions: result,
+    });
+  } catch (error) {
+    return errorHandler(error, res);
+  }
 };
 
-export default withMethods(["POST"], handler);
+export default withMethods(["POST"], withUserAuthorized(handler));
--- src/services/cms/bunny/BunnyCMS.ts
+++ src/services/cms/bunny/BunnyCMS.ts
@@ -8,28 +8,28 @@ export interface BunnyConfig extends ICMSConfig {
   secretKey: string;
 }
 export class BunnyCMS implements IContentProvider<BunnyConfig> {
-
-
   provider: string = "bunny.net";
 
-
   /**
-   * 
-   * @param config 
+   *
+   * @param config
    * @returns true if Access Key is valid, throws error if it fails
    */
   testConfiguration(config: BunnyConfig): Promise<Boolean | APIServerError> {
     const bunny = new BunnyClient(config.accessKey);
-    return bunny.listVideoLibraries().then(r => {
-      if (r.status == 200) {
-        return new Boolean(true)
-      } else {
-        return r as APIServerError;
-      }
-    }).catch(err => new APIServerError(err))
+    return bunny
+      .listVideoLibraries()
+      .then((r) => {
+        if (r.status == 200) {
+          return new Boolean(true);
+        } else {
+          return r as APIServerError;
+        }
+      })
+      .catch((err) => new APIServerError(err));
   }
 
-  listReplicationRegions(): Promise<{ name: string; code: string; }[]> {
+  listReplicationRegions(): Promise<{ name: string; code: string }[]> {
     return new Promise((resolve, reject) => {
       resolve([
         { name: "London, UK", code: "UK" },
@@ -41,8 +41,30 @@ export class BunnyCMS implements IContentProvider<BunnyConfig> {
         { name: "Los Angeles, US West", code: "LA" },
         { name: "Falkenstein, Europe", code: "DE" },
         { name: "Stockholm, Europe", code: "SY" },
-      ])
-    })
+      ]);
+    });
   }
 
+  /**
+   * Saves the configuration to database
+   * @param authConfig
+   * @param replicatedRegions
+   * @param allowedDomains
+   * @param videoResolutions
+   * @param watermarkFile
+   */
+  saveVODConfig(
+    authConfig: BunnyConfig,
+    replicatedRegions: { name: string; code: string }[],
+    allowedDomains: string[],
+    videoResolutions: string[],
+    watermarkFile?: Buffer
+  ): Promise<Boolean | APIServerError> {
+    //create a Bunny client and create a video library with the above settings
+
+    //Update the video library based on the above configuration
+
+    //Save the video library and accessKey in the database
+    throw new Error("Method not implemented.");
+  }
 }
