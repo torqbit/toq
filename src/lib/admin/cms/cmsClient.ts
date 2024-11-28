a460579212b9a6f9e36a5072f13b7493786f32c2
mehrab-alam <mehrab2000alam@gmail.com>
2024-11-28 12:55:00 +0530

    feat: Integrated the UI with saved values

--- src/lib/admin/cms/cmsClient.ts
+++ src/lib/admin/cms/cmsClient.ts
@@ -1,15 +1,27 @@
 import { VODConfig } from "@/pages/api/v1/admin/config/cms/vod";
 import { postFetch } from "@/services/request";
+import { BunnyCMSConfig } from "@/types/cms/bunny";
+import { ConfigurationState } from "@prisma/client";
 
 interface ApiResponse {
   success: boolean;
   error: string;
   message: string;
+  config: { config: BunnyCMSConfig; state: ConfigurationState };
   regions: { name: string; code: string }[];
 }
 
 type FailedApiResponse = {
   error: string;
+};
+
+type VODClientConfig = {
+  provider: string;
+  brandName: string;
+  replicatedRegions: string[];
+  videoResolutions: string[];
+  playerColor: string;
+  watermarkUrl?: string;
 };
 class cmsClient {
   testAccessKey = (
@@ -33,8 +45,32 @@ class cmsClient {
     });
   };
 
-  addVod = (data: VODConfig, onSuccess: (response: ApiResponse) => void, onFailure: (message: string) => void) => {
+  addVod = (
+    data: VODClientConfig,
+    onSuccess: (response: ApiResponse) => void,
+    onFailure: (message: string) => void
+  ) => {
     postFetch(data, `/api/v1/admin/config/cms/vod`).then((result) => {
+      if (result.status == 200 || result.status == 201) {
+        result.json().then((r) => {
+          const apiResponse = r as ApiResponse;
+          onSuccess(apiResponse);
+        });
+      } else {
+        result.json().then((r) => {
+          const failedResponse = r as FailedApiResponse;
+          onFailure(failedResponse.error);
+        });
+      }
+    });
+  };
+
+  getConfigDetail = (
+    provider: string,
+    onSuccess: (response: ApiResponse) => void,
+    onFailure: (message: string) => void
+  ) => {
+    postFetch({ provider }, `/api/v1/admin/config/cms/get`).then((result) => {
       if (result.status == 200 || result.status == 201) {
         result.json().then((r) => {
           const apiResponse = r as ApiResponse;
