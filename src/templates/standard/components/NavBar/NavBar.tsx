6760bba41ce39df27afe2349e2b469292dedd9bc
mehrab-alam <mehrab2000alam@gmail.com>
2024-12-10 18:04:51 +0530

    fixed: Added templates directory

---
+++ src/templates/standard/components/NavBar/NavBar.tsx
@@ -0,0 +1,65 @@
+import { FC, ReactElement } from "react";
+import Image from "next/image";
+import { Button, Flex } from "antd";
+import Link from "next/link";
+import { INavBarProps } from "@/types/landing/navbar";
+import styles from "./NavBar.module.scss";
+import ThemeSwitch from "@/components/ThemeSwitch/ThemeSwitch";
+import MobileNav from "./SideNavBar";
+
+const NavBar: FC<INavBarProps> = ({ user, items, brand, showThemeSwitch, activeTheme, isMobile }): ReactElement => {
+  return (
+    <section className={styles.navigation_main_container}>
+      {isMobile ? (
+        <MobileNav
+          items={items}
+          showThemeSwitch={showThemeSwitch}
+          activeTheme={activeTheme}
+          brand={brand}
+          isMobile={isMobile}
+        />
+      ) : (
+        <div className={styles.navBarContainer}>
+          <nav>
+            <Link href={"/"} aria-label="Go back to landing page">
+              <Flex align="center" gap={5}>
+                {typeof brand.logo === "string" ? (
+                  <Image src={brand.logo} height={40} width={40} style={{ width: "auto" }} alt={"logo"} />
+                ) : (
+                  brand.logo
+                )}
+                {!brand.logo && <h1 className="font-brand">{brand.name}</h1>}
+              </Flex>
+            </Link>
+            <div className={styles.link_wrapper}>
+              {items.length === 0 ? (
+                <div></div>
+              ) : (
+                <ul>
+                  {items.map((navigation, i) => {
+                    return (
+                      <li key={i}>
+                        <Link href={navigation.link} aria-label={`link to ${navigation.title} page`}>
+                          {navigation.title}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+              <Flex align="center" gap={20}>
+                {showThemeSwitch && <ThemeSwitch activeTheme={activeTheme} />}
+
+                <Link href={user ? `/dashboard` : `/login`} aria-label="Get started">
+                  <Button type="primary">{user ? "Go to Dashboard" : "Get Started"}</Button>
+                </Link>
+              </Flex>
+            </div>
+          </nav>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default NavBar;
---
+++ src/templates/standard/components/NavBar/SideNavBar.tsx
@@ -0,0 +1,91 @@
+import React, { FC, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Drawer, Flex } from "antd";
+import styles from "./NavBar.module.scss";
+import { INavBarProps } from "@/types/landing/navbar";
+import ThemeSwitch from "@/components/ThemeSwitch/ThemeSwitch";
+import Hamburger from "hamburger-react";
+
+const MobileNav: FC<INavBarProps> = ({ items, showThemeSwitch, activeTheme, brand }) => {
+  const [showSideNav, setSideNav] = useState(false);
+
+  const onAnchorClick = () => {
+    setSideNav(false);
+  };
+  return (
+    <>
+      <section className={styles.sideNaveContainer}>
+        <Drawer
+          classNames={{ header: styles.drawerHeader }}
+          title={
+            <div className={styles.drawerTitle}>
+              <Link href={"/"} aria-label="Go back to landing page">
+                <Flex align="center" gap={5}>
+                  <Image
+                    src={`${brand.logo}`}
+                    height={40}
+                    width={40}
+                    style={{ width: "auto" }}
+                    alt={"logo"}
+                    loading="lazy"
+                  />
+                  {!brand.logo && <h1 className="font-brand">{brand.name}</h1>}
+                </Flex>
+              </Link>
+              {showSideNav && showThemeSwitch && <ThemeSwitch activeTheme={activeTheme} />}
+            </div>
+          }
+          placement="left"
+          width={300}
+          closable={false}
+          onClose={onAnchorClick}
+          open={showSideNav}
+        >
+          <div className={styles.menuDrawer}>
+            {items &&
+              items.map((item, i) => {
+                return (
+                  <div
+                    key={i}
+                    className={styles.drawerMenuItems}
+                    onClick={() => item.title === "Courses" && onAnchorClick()}
+                  >
+                    {item.title === "Courses" ? (
+                      <a href={item.link} className={styles.menuTitle} aria-label={`link to ${item.title}`}>
+                        <div>{item.title}</div>
+                      </a>
+                    ) : (
+                      <Link key={i} href={item.link} aria-label={`link to ${item.title}`}>
+                        {item.title}
+                      </Link>
+                    )}
+                  </div>
+                );
+              })}
+          </div>
+        </Drawer>
+      </section>
+      <div className={styles.responsive__header}>
+        <Link href={"/"} className={styles.platformNameLogo}>
+          <Flex align="center" gap={5}>
+            <Image src={`${brand.logo}`} height={40} width={40} alt={"logo"} loading="lazy" />
+            <h4 className="font-brand">{brand.name}</h4>
+          </Flex>
+        </Link>
+        <div role="button" className={styles.hamburger} aria-label="Toggle menu">
+          <Hamburger
+            rounded
+            direction="left"
+            toggled={showSideNav}
+            onToggle={(toggle: boolean | ((prevState: boolean) => boolean)) => {
+              setSideNav(toggle);
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MobileNav;
