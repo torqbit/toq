6550644b9914a6324553a80691fbfa73dc4ce257
mehrab-alam <mehrab2000alam@gmail.com>
2024-12-24 12:08:10 +0530

    added course list info in site preview page

--- src/pages/admin/site/preview.tsx
+++ src/pages/admin/site/preview.tsx
@@ -2,14 +2,69 @@ import { getCookieName } from "@/lib/utils";
 import { DEFAULT_THEME, PageSiteConfig } from "@/services/siteConstant";
 import StandardTemplate from "@/templates/standard/StandardTemplate";
 
-import { User } from "@prisma/client";
+import { StateType, User } from "@prisma/client";
 import { GetServerSidePropsContext } from "next";
 import { getToken } from "next-auth/jwt";
 import { FC, useEffect, useState } from "react";
 import { getSiteConfig } from "@/services/getSiteConfig";
+import ProgramService from "@/services/ProgramService";
+import { ICourseCard } from "@/types/landing/courses";
+import { message } from "antd";
+import { convertSecToHourandMin } from "../content";
 
 const PreviewPage: FC<{ user: User; siteConfig: PageSiteConfig }> = ({ user, siteConfig }) => {
   const [config, setConfig] = useState<PageSiteConfig>(siteConfig);
+
+  const [courseList, setCourseList] = useState<ICourseCard[]>([]);
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const getCourseList = () => {
+    ProgramService.getCoursesByAuthor(
+      true,
+      (res) => {
+        let listData =
+          res.courses.length > 0
+            ? res.courses
+                .filter((c) => c.state === StateType.ACTIVE)
+                .map((course: any) => {
+                  let totalDuration = 0;
+                  course.chapters.forEach((chap: any) => {
+                    chap.resource.forEach((r: any) => {
+                      if (r.video) {
+                        totalDuration = totalDuration + r.video?.videoDuration;
+                      } else if (r.assignment) {
+                        totalDuration = totalDuration + Number(r.assignment.estimatedDuration) * 60;
+                      }
+                    });
+                  });
+                  let duration = convertSecToHourandMin(totalDuration);
+                  return {
+                    title: course.name,
+                    thumbnail: course.thumbnail || "",
+                    duration: `${duration}`,
+                    description: course.description,
+                    link: `/courses/${course.slug}`,
+                    courseType: course.courseType,
+                    price: Number(course.coursePrice),
+                    difficulty: course.difficultyLevel,
+                  };
+                })
+            : [];
+
+        setCourseList(listData);
+      },
+      (err) => {
+        messageApi.error(`Unable to get the courses due to ${err}`);
+      }
+    );
+  };
+
+  useEffect(() => {
+    if (siteConfig.sections?.courses?.enable) {
+      getCourseList();
+    }
+  }, []);
+
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
       if (event.data.type === "SITE_CONFIG") {
@@ -24,7 +79,12 @@ const PreviewPage: FC<{ user: User; siteConfig: PageSiteConfig }> = ({ user, sit
     };
   }, []);
 
-  return <StandardTemplate user={user} siteConfig={config} previewMode courseList={[]} />;
+  return (
+    <>
+      {contextHolder}
+      <StandardTemplate user={user} siteConfig={config} previewMode courseList={courseList} />
+    </>
+  );
 };
 export default PreviewPage;
 
