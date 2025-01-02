7d0f4c72e72eb5e348c5a1bd47f4b3b3d8a44b2c
mehrab-alam <mehrab2000alam@gmail.com>
2025-01-02 15:57:11 +0530

    undo dark theme config changes

--- src/services/darkThemeConfig.ts
+++ src/services/darkThemeConfig.ts
@@ -158,7 +158,7 @@ const darkThemeConfig = (siteConfig: PageSiteConfig) => {
         colorBgElevated: "#283040",
       },
       Collapse: {
-        headerBg: "#202433",
+        headerBg: "#283040",
         colorText: "#939db8",
       },
       Table: {
