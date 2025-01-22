c192cc7cb77b77318544aeba4e87d346bd6306c3
Mehrab <mehrab2000alam@gmail.com>
2025-01-22 14:08:21 +0530

    feat: Added  APIs for learning path (#723)
    * feat: Added  APIs for learning path
    
    * fixed the response message for delete
    
    * added a field in learing path schema
    
    * fixed the spelling mistakes
    
    * fixed spelling mistakes

---
+++ src/pages/api/v1/learningPath/list.ts
@@ -0,0 +1,77 @@
+import prisma from "@/lib/prisma";
+import { NextApiResponse, NextApiRequest } from "next";
+import { errorHandler } from "@/lib/api-middlewares/errorHandler";
+import { withMethods } from "@/lib/api-middlewares/with-method";
+import { withAuthentication } from "@/lib/api-middlewares/with-authentication";
+import { Role, StateType } from "@prisma/client";
+import { getCookieName } from "@/lib/utils";
+import { getToken } from "next-auth/jwt";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    let cookieName = getCookieName();
+
+    const token = await getToken({
+      req,
+      secret: process.env.NEXT_PUBLIC_SECRET,
+      cookieName,
+    });
+
+    if (token?.role == Role.ADMIN) {
+      const learningPathList = await prisma.learningPath.findMany({
+        select: {
+          title: true,
+          description: true,
+          state: true,
+          banner: true,
+          author: {
+            select: {
+              name: true,
+            },
+          },
+          learningPathCourses: {
+            select: {
+              courseId: true,
+            },
+          },
+        },
+      });
+      return res.status(200).json({
+        success: true,
+        message: "Learning path list has been fetched",
+        learningPathList,
+      });
+    } else {
+      const learningPathList = await prisma.learningPath.findMany({
+        where: {
+          state: StateType.ACTIVE,
+        },
+        select: {
+          title: true,
+          description: true,
+          state: true,
+          banner: true,
+          author: {
+            select: {
+              name: true,
+            },
+          },
+          learningPathCourses: {
+            select: {
+              courseId: true,
+            },
+          },
+        },
+      });
+      return res.status(200).json({
+        success: true,
+        message: "Learning path list has been fetched",
+        learningPathList,
+      });
+    }
+  } catch (error) {
+    return errorHandler(error, res);
+  }
+};
+
+export default withMethods(["GET"], withAuthentication(handler));
