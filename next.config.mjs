01b1100ef9d5abb7c97c93c60fe74e5816e3a45b
mehrab-alam <mehrab2000alam@gmail.com>
2025-02-12 15:57:14 +0530

    chore: Migrated to next 14

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,49 @@
 /** @type {import('next').NextConfig} */
 
 const { protocol, hostname } = new URL(process.env.NEXTAUTH_URL || "http://localhost:3000");
-
-const cacheConfig = require("./cacheConfig");
+import pwa from "next-pwa";
+import cacheConfig from "./cacheConfig.mjs";
 
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [
+    // antd & deps
+    "@ant-design",
+    "@rc-component",
+    "antd",
+    "rc-cascader",
+    "rc-checkbox",
+    "rc-collapse",
+    "rc-dialog",
+    "rc-drawer",
+    "rc-dropdown",
+    "rc-field-form",
+    "rc-image",
+    "rc-input",
+    "rc-input-number",
+    "rc-mentions",
+    "rc-menu",
+    "rc-motion",
+    "rc-notification",
+    "rc-pagination",
+    "rc-picker",
+    "rc-progress",
+    "rc-rate",
+    "rc-resize-observer",
+    "rc-segmented",
+    "rc-select",
+    "rc-slider",
+    "rc-steps",
+    "rc-switch",
+    "rc-table",
+    "rc-tabs",
+    "rc-textarea",
+    "rc-tooltip",
+    "rc-tree",
+    "rc-tree-select",
+    "rc-upload",
+    "rc-util",
+  ],
   images: {
     remotePatterns: [
       {
@@ -47,13 +85,12 @@ const nextConfig = {
   },
 };
 
-const withPWA = require("next-pwa")({
+const withPWA = pwa({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
-
   runtimeCaching: cacheConfig,
 });
 
-module.exports = withPWA(nextConfig);
+export default withPWA(nextConfig);
