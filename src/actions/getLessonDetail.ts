e454dc0a02f16d7aef7fca63ed450abe77a054ca
Mehrab <mehrab2000alam@gmail.com>
2025-02-17 16:07:59 +0530

    fixed: Updated the state as completed when all courses of learning path completed (#878)

--- src/actions/getLessonDetail.ts
+++ src/actions/getLessonDetail.ts
@@ -1,27 +1,69 @@
 import { ILessonPreviewDetail } from "@/types/courses/Course";
 import { $Enums, CourseState, Role, StateType } from "@prisma/client";
 import prisma from "@/lib/prisma";
-
-const lessonDetailForStudent = async (
+const lessonDetailForStudentByLearningPath = async (
+  learningPathId: number,
   courseId: number,
-  userId: string
-): Promise<{ lessonDetail: ILessonPreviewDetail[]; userRole: string }> => {
-  const isRegistered = await prisma.courseRegistration.findFirst({
-    where: {
-      studentId: userId,
-      order: {
-        productId: courseId,
-      },
-    },
-    select: {
-      courseState: true,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
 
-  if (isRegistered && isRegistered.courseState == CourseState.COMPLETED) {
+  userId: string,
+
+  courseState?: CourseState
+): Promise<{ lessonDetail: ILessonPreviewDetail[]; userRole: string }> => {
+  if (courseState && courseState == CourseState.COMPLETED) {
+    return new Promise(async (resolve, reject) => {
+      let resultRows = await prisma.$queryRaw<
+        ILessonPreviewDetail[]
+      >`SELECT DISTINCT  ch.sequenceId as chapterSeq, re.sequenceId as resourceSeq,sub.status as assignmentStatus , re.resourceId, re.name as lessonName, co.name as courseName, co.description,co.previewMode,
+  re.description as lessonDescription, vi.id as videoId, vi.videoUrl, vi.videoDuration, ch.chapterId,re.contentType as contentType ,assign.estimatedDuration,
+  ch.name as chapterName, cp.resourceId as watchedRes FROM LearningPath as lp
+  INNER JOIN LearningPathCourses as lpc ON lpc.learningPathId = lp.id
+  INNER JOIN Course as co ON co.courseId = lpc.courseId
+  INNER JOIN \`Order\` as o ON o.productId = lp.id
+  INNER JOIN CourseRegistration as cr ON cr.orderId = o.id
+  INNER JOIN Chapter as ch ON co.courseId = ch.courseId
+  INNER JOIN Resource as re ON ch.chapterId = re.chapterId
+  LEFT OUTER JOIN Assignment as assign ON re.resourceId = assign.lessonId
+  LEFT OUTER JOIN Video as vi ON re.resourceId = vi.resourceId
+  LEFT OUTER JOIN AssignmentSubmission as sub ON re.resourceId = sub.lessonId AND sub.studentId = o.studentId
+  INNER JOIN CourseProgress as cp ON cp.studentId = o.studentId  AND cp.resourceId = re.resourceId
+  WHERE  re.state = ${$Enums.StateType.ACTIVE}  AND ch.state = ${StateType.ACTIVE}
+  AND lp.id = ${learningPathId} AND co.courseId = ${courseId}  AND o.studentId = ${userId}
+  ORDER BY chapterSeq, resourceSeq;`;
+
+      resolve({ lessonDetail: resultRows, userRole: Role.STUDENT });
+    });
+  } else {
+    return new Promise(async (resolve, reject) => {
+      let resultRows = await prisma.$queryRaw<
+        ILessonPreviewDetail[]
+      >`SELECT DISTINCT  ch.sequenceId as chapterSeq, re.sequenceId as resourceSeq,sub.status as assignmentStatus , re.resourceId, re.name as lessonName, co.name as courseName, co.description,co.previewMode,
+  re.description as lessonDescription, vi.id as videoId, vi.videoUrl, vi.videoDuration, ch.chapterId,re.contentType as contentType ,assign.estimatedDuration,
+  ch.name as chapterName, cp.resourceId as watchedRes FROM LearningPath as lp
+ INNER JOIN LearningPathCourses as lpc ON lpc.learningPathId = lp.id
+  INNER JOIN Course as co ON co.courseId = lpc.courseId
+  INNER JOIN \`Order\` as o ON o.productId = lp.id
+  INNER JOIN CourseRegistration as cr ON cr.orderId = o.id
+  INNER JOIN Chapter as ch ON co.courseId = ch.courseId
+  INNER JOIN Resource as re ON ch.chapterId = re.chapterId
+  LEFT OUTER JOIN Assignment as assign ON re.resourceId = assign.lessonId
+  LEFT OUTER JOIN Video as vi ON re.resourceId = vi.resourceId
+  LEFT OUTER JOIN AssignmentSubmission as sub ON re.resourceId = sub.lessonId AND sub.studentId = o.studentId
+  LEFT OUTER JOIN CourseProgress as cp ON cp.studentId = o.studentId  AND cp.resourceId = re.resourceId
+  WHERE  re.state = ${$Enums.StateType.ACTIVE}  AND ch.state = ${StateType.ACTIVE}
+  AND lp.id = ${learningPathId} AND co.courseId = ${courseId}  AND o.studentId = ${userId}
+  ORDER BY chapterSeq, resourceSeq;`;
+
+      resolve({ lessonDetail: resultRows, userRole: Role.STUDENT });
+    });
+  }
+};
+const lessonDetailForStudentByCourse = async (
+  productId: number,
+
+  userId: string,
+  courseState?: CourseState
+): Promise<{ lessonDetail: ILessonPreviewDetail[]; userRole: string }> => {
+  if (courseState && courseState == CourseState.COMPLETED) {
     return new Promise(async (resolve, reject) => {
       let resultRows = await prisma.$queryRaw<
         ILessonPreviewDetail[]
@@ -37,7 +79,7 @@ const lessonDetailForStudent = async (
   LEFT OUTER JOIN AssignmentSubmission as sub ON re.resourceId = sub.lessonId AND sub.studentId = o.studentId
   INNER JOIN CourseProgress as cp ON cp.studentId = o.studentId  AND cp.resourceId = re.resourceId
   WHERE  re.state = ${$Enums.StateType.ACTIVE}  AND ch.state = ${StateType.ACTIVE}
-  AND co.courseId = ${courseId}  AND o.studentId = ${userId}
+  AND co.courseId = ${productId}  AND o.studentId = ${userId}
   ORDER BY chapterSeq, resourceSeq;`;
 
       resolve({ lessonDetail: resultRows, userRole: Role.STUDENT });
@@ -58,11 +100,38 @@ const lessonDetailForStudent = async (
   LEFT OUTER JOIN AssignmentSubmission as sub ON re.resourceId = sub.lessonId AND sub.studentId = o.studentId
   LEFT OUTER JOIN CourseProgress as cp ON cp.studentId = o.studentId  AND cp.resourceId = re.resourceId
   WHERE  re.state = ${$Enums.StateType.ACTIVE}  AND ch.state = ${StateType.ACTIVE}
-  AND co.courseId = ${courseId}  AND o.studentId = ${userId}
+  AND co.courseId = ${productId}  AND o.studentId = ${userId}
   ORDER BY chapterSeq, resourceSeq;`;
 
       resolve({ lessonDetail: resultRows, userRole: Role.STUDENT });
     });
+  }
+};
+
+const lessonDetailForStudent = async (
+  courseId: number,
+  userId: string,
+  learningPathId?: number
+): Promise<{ lessonDetail: ILessonPreviewDetail[]; userRole: string }> => {
+  const isRegistered = await prisma.courseRegistration.findFirst({
+    where: {
+      studentId: userId,
+      order: {
+        productId: learningPathId ? learningPathId : courseId,
+      },
+    },
+    select: {
+      courseState: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  if (learningPathId) {
+    return lessonDetailForStudentByLearningPath(learningPathId, courseId, userId, isRegistered?.courseState);
+  } else {
+    return lessonDetailForStudentByCourse(courseId, userId, isRegistered?.courseState);
   }
 };
 const lessonDetail = async (
@@ -89,7 +158,7 @@ ORDER BY chapterSeq, resourceSeq;`;
   });
 };
 
-const getLessonDetailForAuthor = async (courseId: number, userId: string, userRole: Role) => {
+const getLessonDetailForAuthor = async (courseId: number, userId: string, userRole: Role, learningPathId?: number) => {
   const getCourseDetail = await prisma.course.findUnique({
     where: {
       courseId: courseId,
@@ -101,20 +170,20 @@ const getLessonDetailForAuthor = async (courseId: number, userId: string, userRo
   if (userId === getCourseDetail?.authorId || userRole === Role.ADMIN) {
     return lessonDetail(courseId, String(userId), Role.AUTHOR);
   } else {
-    return lessonDetailForStudent(courseId, String(userId));
+    return lessonDetailForStudent(courseId, String(userId), learningPathId);
   }
 };
 
-const getLessonDetail = async (courseId: number, role?: Role, userId?: string) => {
+const getLessonDetail = async (courseId: number, role?: Role, userId?: string, learningPathId?: number) => {
   switch (role) {
     case Role.AUTHOR:
-      return getLessonDetailForAuthor(courseId, String(userId), role);
+      return getLessonDetailForAuthor(courseId, String(userId), role, learningPathId);
     case Role.ADMIN:
-      return getLessonDetailForAuthor(courseId, String(userId), role);
+      return getLessonDetailForAuthor(courseId, String(userId), role, learningPathId);
     case Role.MENTOR:
       return lessonDetail(courseId, String(userId), Role.MENTOR);
     case Role.STUDENT:
-      return lessonDetailForStudent(courseId, String(userId));
+      return lessonDetailForStudent(courseId, String(userId), learningPathId);
   }
 };
 
