56b205fbaefc06fe3bbfe3f51c12687394035dd3
Shadab Alam <shadab19it@gmail.com>
2025-02-20 13:57:29 +0530

    fixed payment gateway config state checked

--- src/actions/getCurrency.ts
+++ src/actions/getCurrency.ts
@@ -5,7 +5,8 @@ import { ConfigurationState, gatewayProvider } from "@prisma/client";
 export const getCurrency = async (provider: gatewayProvider): Promise<string> => {
   const paymentManager = new PaymentManagemetService();
   const result = await paymentManager.getGatewayConfig(provider);
-  if (result.body.config && result.body.state == ConfigurationState.PAYMENT_CONFIGURED) {
+
+  if (result.success && result.body.config && result.body.state == ConfigurationState.PAYMENT_CONFIGURED) {
     return result.body.config.currency;
   } else {
     return appConstant.currency;
